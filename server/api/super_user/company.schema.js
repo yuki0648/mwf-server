4f3e8ccded8ebd0cd2e5fb206fc359f96a5dbd1d
SCYtesting <spyephy@Gmail.com>
2016-08-23 15:53:34 +0800

    Yuki 2016-08*23

--- server/api/super_user/company.schema.js
+++ server/api/super_user/company.schema.js
@@ -2,19 +2,18 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var Promise = require('bluebird');
 
-var projectSchema = new Schema({
-  name: String,
-  projectNo: String,
-  description:String,
-  address: String,
-  type: String,
+var companySchema = new Schema({
+  id:String,
+  chiname: String,
+  engname: String,
+  code: String,
+  status:String,
   assigned_date:{ type: Date, default: Date.now('YYYY-MM-DDT00:00:00Z')},
-  start_date:Date,
-  end_date:Date,
-  staff:[String]
+  department:[{_id:String,name:String}],
+  description: String
 });
 
-model = mongoose.model('Project', projectSchema);
+model = mongoose.model('Company', companySchema);
 Promise.promisifyAll(model);
 
 module.exports = model
